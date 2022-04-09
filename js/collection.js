17917c8dd6ae92a49b4a954e8004cba3112d6398
Shangzhen Yang <shangzheny01@gmail.com>
2022-04-08 17:05:57 -0700

    Add collection

---
+++ js/collection.js
@@ -0,0 +1,10 @@
+const collection = Vue.createApp({
+	data() {
+		return {
+			badges: {
+				Drink: ["Starbucks", "Coco", "ShareTea", "Boba Up"],
+				Food: ["Ramen", "Sushi", "Hamburger", "Steak"]
+			}
+		}
+	}
+}).mount("main")

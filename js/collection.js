dd4123a6bc39bebf67d1d6a9c26b5a8326d18f80
shanyitong <shanyitong@yahoo.com>
2022-04-08 17:22:58 -0700

    sticker collection

--- js/collection.js
+++ js/collection.js
@@ -2,7 +2,13 @@ const collection = Vue.createApp({
 	data() {
 		return {
 			badges: {
-				Drink: ["Starbucks", "Coco", "ShareTea", "Boba Up"],
+				Drink: ["DawGo%20Stickers/drink1.png",
+				 			"DawGo%20Stickers/drink2.png",
+							"DawGo%20Stickers/drink3.png",
+				   			"DawGo%20Stickers/drink4.png",
+							"DawGo%20Stickers/drink5.png",
+							"DawGo%20Stickers/drink6.png"
+						],
 				Food: ["Ramen", "Sushi", "Hamburger", "Steak"]
 			}
 		}

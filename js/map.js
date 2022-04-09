7b10b18edb2cb99fc36438949ed757ded630d112
Shangzhen Yang <shangzheny01@gmail.com>
2022-04-08 17:02:52 -0700

    Add collection

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-let map;
+let map, infoWindow;
 function initMap() {
 	map = new google.maps.Map(document.getElementById("map"), {
 		center: {
@@ -7,5 +7,23 @@ function initMap() {
 		},
 		mapId: "d470e0ed1c5fa7dd",
 		zoom: 12
-	});
+	})
+	infoWindow = new google.maps.InfoWindow()
+}
+if (navigator.geolocation) {
+	navigator.geolocation.getCurrentPosition(position => {
+		const pos = {
+			lat: position.coords.latitude,
+			lng: position.coords.longitude,
+		}
+		infoWindow.setPosition(pos)
+		infoWindow.setContent("You are here! You are not in any store!")
+		infoWindow.open(map)
+		map.setCenter(pos)
+		map.setZoom(16)
+	}, () => {
+		handleLocationError(true, infoWindow, map.getCenter())
+	})
+} else {
+	handleLocationError(false, infoWindow, map.getCenter())
 }

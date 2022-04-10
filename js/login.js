8c359e4005e637f5e9751423efe2f4a5d09a0ca2
Shangzhen Yang <shangzheny01@gmail.com>
2022-04-09 21:44:35 -0700

    Fix mobile login layout

--- js/login.js
+++ js/login.js
@@ -1,6 +1,6 @@
 (() => {
 	let isSignupPage = false
-	document.getElementById("submit-btn").onclick = () => {
+	document.getElementById("btn-submit").onclick = () => {
 		const email = document.getElementById("email").value
 		const password = document.getElementById("password").value
 		if (email === "lucyjoe@example.com" && password === "123456") {
@@ -14,12 +14,12 @@
 		isSignupPage = !isSignupPage
 		if (isSignupPage) {
 			this.innerText = "I have an account"
-			document.getElementById("submit-btn").value = "Sign Up"
+			document.getElementById("btn-submit").value = "Sign Up"
 			document.getElementById("confirm-password").classList.remove("hidden")
 		}
 		else {
 			this.innerText = "Sign Up"
-			document.getElementById("submit-btn").value = "Login"
+			document.getElementById("btn-submit").value = "Login"
 			document.getElementById("confirm-password").classList.add("hidden")
 		}
 	}
